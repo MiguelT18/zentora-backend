700bea9c0eb2ad3d7b3abe40defa6e36e38b4fe2
Miguel Teran <miguel@cyberjunk.>
2025-09-12 04:52:33 -0400

    Agregar .env con DEBUG y configurar main.py para ejecutar Uvicorn en 0.0.0.0 con reload según DEBUG

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 from app.api.v1.routes import router as api_v1_router
 
 app = FastAPI(title="Zentora API", version="0.1.0")
 
 app.include_router(api_v1_router, prefix="/api/v1")
+
+if __name__ == "__main__":
+  uvicorn.run(
+    "app.main:app",
+    host="0.0.0.0",
+    port=8000,
+    reload=True if os.getenv("DEBUG") == True else False
+  )
