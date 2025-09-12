c89bbd104dd6f120b7700d7577009b144c388629
Miguel Teran <miguel@cyberjunk.>
2025-09-12 05:00:01 -0400

    Agregar __init__.py en app y corregir import y run de main.py para que funcione correctamente

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import os
 import uvicorn
 from fastapi import FastAPI
-from app.api.v1.routes import router as api_v1_router
+from api.v1.routes import router as api_v1_router
 
 app = FastAPI(title="Zentora API", version="0.1.0")
 
@@ -9,7 +9,7 @@ app.include_router(api_v1_router, prefix="/api/v1")
 
 if __name__ == "__main__":
   uvicorn.run(
-    "app.main:app",
+    "__main__:app",
     host="0.0.0.0",
     port=8000,
     reload=True if os.getenv("DEBUG") == True else False
