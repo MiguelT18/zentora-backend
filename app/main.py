f0dcfbf8a13aa8189af386681a10f97dfcf5c202
Miguel Teran <miguel@cyberjunk.>
2025-09-13 00:36:25 -0400

    feat: añadir endpoint de health check y corregir puerto en uvicorn
    - Se agregó un endpoint en / que devuelve {status: ok} para verificar la disponibilidad del backen (útil para Render y monitoreo).
    - Se corrigió la configuración de uvicor convirtiendo la variable de entorno PORT a entero con  para evitar errores de logging

--- app/main.py
+++ app/main.py
@@ -7,10 +7,14 @@ app = FastAPI(title="Zentora API", version="0.1.0")
 
 app.include_router(api_v1_router, prefix="/api/v1")
 
+@app.get("/")
+async def health_check():
+  return {"status": "ok"}
+
 if __name__ == "__main__":
   uvicorn.run(
     "__main__:app",
     host="0.0.0.0",
-    port=os.getenv("PORT", 8000),
+    port=int(os.getenv("PORT", 8000)),
     reload=True if os.getenv("DEBUG") == True else False
   )
